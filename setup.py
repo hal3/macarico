ae73e5344bfc63f7eb2dacc22d4d61914d9da08a
Tim Vieira <tim.f.vieira@gmail.com>
2017-04-19 15:11:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(name='macarico',
       version='0.0.1',
       description='',
-      author=u'Hal Daumé III Tim Vieira',
+      author=u'Hal Daumé III and Tim Vieira',
       packages=['macarico'],
       install_requires=[
           #'pytorch' <- should install via conda, not pip.
