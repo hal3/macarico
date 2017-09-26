082d060129383a2764ac58372071f19b1039235f
amrsharaf <amr.n.sharaf@gmail.com>
2017-09-26 19:03:33 -0400

    linking BootstrapPolicy class to utility functions

--- macarico/policies/bootstrap.py
+++ macarico/policies/bootstrap.py
@@ -1,12 +1,15 @@
 from __future__ import division
 
+from macarico.policies.linear import LinearPolicy
 from macarico import Policy
 import numpy as np
+import dynet as dy
+
 
 # Randomize over predictions from a base set of predictors
 def bootstrap_probabilities(num_actions, bag_size, policy_bag, state):
     preds = np.zeros(num_actions)
-    prob = 1. / bag_size;
+    prob = 1. / bag_size
     for policy in policy_bag:
         # get the prediction
         a = policy(state)
@@ -15,9 +18,13 @@ def bootstrap_probabilities(num_actions, bag_size, policy_bag, state):
     return preds
 
 
-# Constructs a policy bag of linear policies, number of policies = bag_size
-def build_policy_bag(bag_size):
-    return [LinearPolicy() for i in range(bag_size)]
+# Constructs a policy bag of linear policies, number of policies =
+# len(features_bag)
+def build_policy_bag(dy_model, features_bag, n_actions, loss_fn):
+    # TODO do we need to deep copy the dy_model?
+    return [LinearPolicy(dy_model, features, n_actions, loss_fn)
+            for features in features_bag]
+
 
 class BootstrapPolicy(Policy):
     """
@@ -25,16 +32,16 @@ class BootstrapPolicy(Policy):
         TODO: how can we train this policy?
     """
 
-    def __init__(self, dy_model, features, n_actions, bag_size=16, loss_fn='squared'):
-        self.policy_bag = None
+    def __init__(self, dy_model, features_bag, n_actions, loss_fn):
+        self.n_actions = n_actions
+        self.bag_size = len(features_bag)
+        self.policy_bag = build_policy_bag(dy_model, features_bag, n_actions,
+                                           loss_fn)
         return None
 
     def __call__(self, state, deviate_to=None):
+        action_probs = bootstrap_probabilities(self.n_actions, self.bag_size,
+                                               self.policy_bag, state)
+        print('Action probabilities: ', action_probs)
         return None
 
-if __name__ == '__main__':
-    num_actions = 10
-    bag_size = 16
-    policy_bag = []
-    state = None
-    print(bootstrap_probabilities(num_actions, bag_size, policy_bag, state))
