e6d56f2f2646ac69b08a4667aeae627aa4d1e04a
amrsharaf <amr.n.sharaf@gmail.com>
2017-09-26 14:18:23 -0400

    function for computing bootstrap probabilities

--- macarico/policies/bootstrap.py
+++ macarico/policies/bootstrap.py
@@ -1,6 +1,18 @@
 from __future__ import division
 
 from macarico import Policy
+import numpy as np
+
+# Randomize over predictions from a base set of predictors
+def bootstrap_probabilities(num_actions, bag_size, policy_bag, state):
+    preds = np.zeros(num_actions)
+    prob = 1. / bag_size;
+    for policy in policy_bag:
+        # get the prediction
+        a = policy(state)
+        # update probability scores
+        preds[a] += prob
+    return preds
 
 
 class BootstrapPolicy(Policy):
@@ -12,3 +24,10 @@ class BootstrapPolicy(Policy):
 
     def __call__(self, state, deviate_to=None):
         return None
+
+if __name__ == '__main__':
+    num_actions = 10
+    bag_size = 16
+    policy_bag = []
+    state = None
+    print(bootstrap_probabilities(num_actions, bag_size, policy_bag, state))
