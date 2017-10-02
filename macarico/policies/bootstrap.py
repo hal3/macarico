b427ad6ecb0a7918e3d061f6bedddb5da7ca475a
amrsharaf <amr.n.sharaf@gmail.com>
2017-10-02 09:08:31 -0400

    refactoring bootstrap sampling code

--- macarico/policies/bootstrap.py
+++ macarico/policies/bootstrap.py
@@ -52,57 +52,13 @@ class BootstrapCost:
         else:
             return dy.average(self.costs).__neg__()
 
-# Sampling from Poisson with rate 1
-def poisson_sample():
-    temp = np.random.random()
-    if(temp <= 0.3678794411714423215955):
-        return 0
-    if(temp <= 0.735758882342884643191):
-        return 1
-    if(temp <= 0.919698602928605803989):
-        return 2
-    if(temp <= 0.9810118431238461909214):
-        return 3
-    if(temp <= 0.9963401531726562876545):
-        return 4
-    if(temp <= 0.9994058151824183070012):
-        return 5
-    if(temp <= 0.9999167588507119768923):
-        return 6
-    if(temp <= 0.9999897508033253583053):
-        return 7
-    if(temp <= 0.9999988747974020309819):
-        return 8
-    if(temp <= 0.9999998885745216612793):
-        return 9
-    if(temp <= 0.9999999899522336243091):
-        return 10
-    if(temp <= 0.9999999991683892573118):
-        return 11
-    if(temp <= 0.9999999999364022267287):
-        return 12
-    if(temp <= 0.999999999995480147453):
-        return 13
-    if(temp <= 0.9999999999996999989333):
-        return 14
-    if(temp <= 0.9999999999999813223654):
-        return 15
-    if(temp <= 0.9999999999999989050799):
-        return 16
-    if(temp <= 0.9999999999999999393572):
-        return 17
-    if(temp <= 0.999999999999999996817):
-        return 18
-    if(temp <= 0.9999999999999999998412):
-        return 19
-    return 20
-
 
 # Constructs a policy bag of linear policies, number of policies =
 # len(features_bag)
 def build_policy_bag(dy_model, features_bag, n_actions, loss_fn):
     return [LinearPolicy(dy_model, features, n_actions, loss_fn)
             for features in features_bag]
+
 
 class BootstrapPolicy(Policy):
     """
@@ -140,6 +96,6 @@ class BootstrapPolicy(Policy):
             if self.greedy_update and idx == 0:
                 count_i = 1
             else:
-                count_i = poisson_sample()
+                count_i = np.random.poisson(1)
             total_loss = total_loss + count_i * loss_i
         return total_loss
