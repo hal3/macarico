e830ecd5ccb351bb11939b76e59fb4c993e14e65
Hal Daume III <me@hal3.name>
2017-11-12 09:36:37 -0500

    working reslope

--- macarico/lts/reslope.py
+++ macarico/lts/reslope.py
@@ -73,24 +73,35 @@ class Reslope(BanditLOLS):
             self.dev_imp_weight.append(iw)
             self.dev_costs.append(a_costs)
 
-        self.pred_act_cost.append(a_costs[a].data)
+        
+        a_costs_data = a_costs.data if isinstance(a_costs, Var) else \
+                       a_costs.data() if isinstance(a_costs, macarico.policies.bootstrap.BootstrapCost) else \
+                         None
+        self.pred_act_cost.append(a_costs_data.numpy()[a])
         return a
 
 
     def update(self, loss0):
+        total_loss_var = 0.
         for dev_t, dev_a, dev_actions, dev_imp_weight, dev_costs in zip(self.dev_t, self.dev_a, self.dev_actions, self.dev_imp_weight, self.dev_costs):
             if dev_costs is None or dev_imp_weight == 0.:
                 continue
+            dev_costs_data = dev_costs.data if isinstance(dev_costs, Var) else \
+                             dev_costs.data() if isinstance(dev_costs, macarico.policies.bootstrap.BootstrapCost) else \
+                             None
+            assert dev_costs_data is not None
 
             loss = loss0 - (sum(self.pred_act_cost) - self.pred_act_cost[dev_t-1])
-            truth = self.build_cost_vector(0, loss, dev_a, dev_imp_weight, dev_costs)
+            truth = self.build_cost_vector(loss, dev_a, dev_imp_weight, dev_costs_data)
             importance_weight = 1
             if self.learning_method in [BanditLOLS.LEARN_MTR, BanditLOLS.LEARN_MTR_ADVANTAGE]:
                 dev_actions = [dev_a if isinstance(dev_a, int) else dev_a.data[0,0]]
                 importance_weight = dev_imp_weight
             loss_var = self.policy.forward_partial_complete(dev_costs, truth, dev_actions)
             loss_var *= importance_weight
-            loss_var.backward()
+            total_loss_var += loss_var
 
             a = dev_a if isinstance(dev_a, int) else dev_a.data[0,0]
-            self.squared_loss = (loss - dev_costs.data[a]) ** 2
+            self.squared_loss = (loss - dev_costs_data[a]) ** 2
+        if not isinstance(total_loss_var, float):
+            total_loss_var.backward()
