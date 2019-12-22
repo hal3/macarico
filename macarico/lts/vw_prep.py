590a5ac6ae73dd9d48293971866e74f170e1f2e5
amrsharaf <amr.n.sharaf@gmail.com>
2019-12-22 13:35:17 +0200

    some cleanup

--- macarico/lts/vw_prep.py
+++ macarico/lts/vw_prep.py
@@ -10,12 +10,10 @@ from macarico.lts.lols import BanditLOLS, LOLS
 
 
 class VwPrep(BanditLOLS):
-    def __init__(self, reference, policy, actor, p_ref=stochastic(NoAnnealing(0)),
-                 exploration=BanditLOLS.EXPLORE_BOLTZMANN, explore=1.0,
+    def __init__(self, reference, policy, actor, exploration=BanditLOLS.EXPLORE_BOLTZMANN, explore=1.0,
                  mixture=LOLS.MIX_PER_ROLL, save_log=False, attach_time=False, expb=0):
         super(VwPrep, self).__init__(policy=policy, reference=reference, exploration=exploration, mixture=mixture,
                                      expb=expb)
-        self.reference = reference
         self.policy = policy
         self.vw_ref_critic = pyvw.vw(quiet=True)
         self.vw_vd_regressor = pyvw.vw(quiet=True)
@@ -23,7 +21,6 @@ class VwPrep(BanditLOLS):
         assert self.exploration in range(BanditLOLS._EXPLORE_MAX), \
             'unknown exploration, must be one of BanditLOLS.EXPLORE_*'
         # TODO handle mixture with ref
-        self.use_ref = p_ref
         self.attach_time = attach_time
         self.init_state = None
         if isinstance(explore, float):
@@ -117,17 +114,16 @@ class VwPrep(BanditLOLS):
         sq_loss = (pred_value - loss0) ** 2
         self.total_sq_loss += sq_loss
         self.vw_ref_critic.learn(ex)
-        if self.dev_t is not None:
-            for dev_t, dev_a, dev_actions, dev_imp_weight, dev_costs, ex in zip(self.dev_t, self.dev_a,
-                                                                                self.dev_actions, self.dev_imp_weight,
-                                                                                self.dev_costs, self.dev_ex):
-                # residual_loss = loss0 - pred_value - (prefix_sum[dev_t-1] - self.pred_act_cost[dev_t-1])
-                # residual_loss = loss0 - pred_value.data.numpy() - (prefix_sum[dev_t-1] - self.pred_act_cost[dev_t-1])
-                bandit_loss = self.pred_act_cost[dev_t-1]
+        assert self.dev_t is not None
+        for dev_t, dev_a, dev_actions, dev_imp_weight, dev_costs, ex in zip(
+                self.dev_t, self.dev_a, self.dev_actions, self.dev_imp_weight, self.dev_costs, self.dev_ex):
+            # residual_loss = loss0 - pred_value - (prefix_sum[dev_t-1] - self.pred_act_cost[dev_t-1])
+            # residual_loss = loss0 - pred_value.data.numpy() - (prefix_sum[dev_t-1] - self.pred_act_cost[dev_t-1])
+            bandit_loss = self.pred_act_cost[dev_t-1]
 #                print('bandit loss: ', bandit_loss)
 #                bandit_loss = loss0
-                self.policy.update(dev_a, bandit_loss, dev_imp_weight, ex)
-                a = dev_a if isinstance(dev_a, int) else dev_a.data[0,0]
+            self.policy.update(dev_a, bandit_loss, dev_imp_weight, ex)
+            a = dev_a if isinstance(dev_a, int) else dev_a.data[0,0]
         # Update VD regressor using all timesteps
         for dev_t in range(self.t):
             residual_loss = loss0 - pred_value - (prefix_sum[dev_t] - self.pred_act_cost[dev_t])
@@ -136,6 +132,5 @@ class VwPrep(BanditLOLS):
 #            print('squared loss: ', vd_sq_loss, ' avg: ', self.total_vd_sq_loss/float(self.counter))
             transition_example = str(residual_loss) + self.transition_ex[dev_t]
             self.vw_vd_regressor.learn(transition_example)
-        self.use_ref.step()
         self.t, self.dev_t, self.dev_a, self.dev_actions, self.dev_imp_weight, self.dev_costs, self.pred_vd, self.pred_act_cost, self.rollout, self.dev_ex, self.transition_ex = [None] * 11
         return total_loss_var
