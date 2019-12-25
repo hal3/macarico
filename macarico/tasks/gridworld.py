6edb80d4487d146edbe440382f8115405ffcbf18
amrsharaf <amr.n.sharaf@gmail.com>
2019-12-25 15:35:54 +0200

    compute reward functions and transition dynamics for grid world

--- macarico/tasks/gridworld.py
+++ macarico/tasks/gridworld.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 
 import macarico
 import macarico.util as util
-from macarico.util import Varng
 
 
 class GridSettings(macarico.Example):
-    def __init__(self, width, height, start, walls, terminals, per_step_cost, max_steps, gamma, p_step_success):
+    def __init__(self, width, height, start, walls, terminals, per_step_cost, max_steps, gamma, p_step_success,
+                 do_break=True):
         super().__init__()
         self.width = width
         self.height = height
@@ -20,6 +20,7 @@ class GridSettings(macarico.Example):
         self.gamma = gamma
         self.p_step_success = p_step_success
         self.n_actions = 4
+        self.do_break = do_break
 
 
 def make_default_gridworld(per_step_cost=0.05, max_steps=50, gamma=0.99, p_step_success=0.8, start_random=False):
@@ -35,7 +36,7 @@ def make_default_gridworld(per_step_cost=0.05, max_steps=50, gamma=0.99, p_step_
                                   per_step_cost, max_steps, gamma, p_step_success))
 
 
-def make_debug_gridworld(per_step_cost=0.05, max_steps=6, gamma=1.0, p_step_success=1.0, start_random=False):
+def make_debug_gridworld(per_step_cost=0.05, max_steps=50, gamma=0.9, p_step_success=1.0, start_random=False):
     #    0123
     #   0   +
     #   1 # -
@@ -44,8 +45,9 @@ def make_debug_gridworld(per_step_cost=0.05, max_steps=6, gamma=1.0, p_step_succ
     start = (0, 3)
     if start_random:
         start = (random.randint(0, 3), random.randint(0, 3))
+    do_break = False
     return GridWorld(GridSettings(4, 4, start, {(1, 1), (1, 2)}, {(3, 0): 1, (3, 1): -1},
-                                  per_step_cost, max_steps, gamma, p_step_success))
+                                  per_step_cost, max_steps, gamma, p_step_success, do_break))
 
 
 def make_deterministic_gridworld(per_step_cost=0.05, max_steps=50, gamma=1, p_step_success=1, start_random=False):
@@ -97,7 +99,7 @@ def make_big_gridworld(per_step_cost=0.01, max_steps=200, gamma=0.99, p_step_suc
 
 class GridWorld(macarico.Env):
     UP, LEFT, DOWN, RIGHT = 0, 1, 2, 3
-    
+
     def __init__(self, example):
         self.loc = example.start
         self.discount = 1.
@@ -119,7 +121,8 @@ class GridWorld(macarico.Env):
             if self.loc in self.example.terminal:
                 self.example.reward += self.discount * self.example.terminal[self.loc]
                 self._losses[-1] -= self.discount * self.example.terminal[self.loc]
-                break
+                if self.example.do_break:
+                    break
             self.discount *= self.example.gamma
         return self.output()
 
@@ -152,9 +155,80 @@ class GridWorld(macarico.Env):
             self.loc = new_loc
             
     def is_legal(self, new_loc):
-        return new_loc[0] >= 0 and new_loc[0] < self.example.width and \
-               new_loc[1] >= 0 and new_loc[1] < self.example.height and \
-               new_loc not in self.example.walls
+        return ((new_loc[0] >= 0) and (new_loc[0] < self.example.width)) and \
+               ((new_loc[1] >= 0) and (new_loc[1] < self.example.height)) and \
+               (new_loc not in self.example.walls)
+
+    def get_x_y(self, state_id):
+        y = int(state_id % self.example.height)
+        x = int((state_id - y) / self.example.height)
+        return x, y
+
+    def get_state_id(self, x, y):
+        return x * self.example.height + y
+
+    def x_y_step(self, x, y, a):
+        new_loc = [x, y]
+        if a == self.UP:
+            new_loc[1] -= 1
+        if a == self.DOWN:
+            new_loc[1] += 1
+        if a == self.LEFT:
+            new_loc[0] -= 1
+        if a == self.RIGHT:
+            new_loc[0] += 1
+        new_loc = tuple(new_loc)
+        if self.is_legal(new_loc):
+            return new_loc
+        else:
+            return (x, y)
+
+    def make_step(self, state, action):
+        x, y = self.get_x_y(state)
+        new_x, new_y = self.x_y_step(x, y, action)
+        return self.get_state_id(new_x, new_y)
+
+    def costs_function(self):
+        import numpy as np
+        costs = np.zeros((16, 4))
+        for current_state in range(16):
+            for action in range(4):
+                next_state = self.make_step(current_state, action)
+                loc = self.get_x_y(next_state)
+                costs[current_state, action] = costs[current_state, action] + self.example.per_step_cost
+                if loc in self.example.terminal:
+                    costs[current_state, action] -= self.example.terminal[loc]
+        return costs
+
+    def costs(self, pi):
+        import numpy as np
+        costs = np.zeros(16)
+        for current_state, action_distribution in enumerate(pi):
+            for action, action_probability in enumerate(action_distribution):
+                next_state = self.make_step(current_state, action)
+                loc = self.get_x_y(next_state)
+                costs[current_state] = costs[current_state] + action_probability * self.example.per_step_cost
+                if loc in self.example.terminal:
+                    costs[current_state] -= action_probability * self.example.terminal[loc]
+        return costs
+
+    def transition(self):
+        import numpy as np
+        model = np.zeros((16, 4, 16))
+        for current_state in range(16):
+            for action in range(4):
+                next_state = self.make_step(current_state, action)
+                model[current_state, action, next_state] = 1.0
+        return model
+
+    def model(self, pi):
+        import numpy as np
+        model = np.zeros((16, 16))
+        for current_state, action_distribution in enumerate(pi):
+            for action, action_probability in enumerate(action_distribution):
+                next_state = self.make_step(current_state, action)
+                model[current_state, next_state] += action_probability
+        return model
 
 
 class GridLoss(macarico.Loss):
