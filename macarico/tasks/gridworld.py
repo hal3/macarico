cdb225bfccfee509b3e2f04fba50057c2f60079f
amrsharaf <amr.n.sharaf@gmail.com>
2019-12-25 18:53:14 +0200

    policy evaluation with dynamic programming

--- macarico/tasks/gridworld.py
+++ macarico/tasks/gridworld.py
@@ -230,6 +230,48 @@ class GridWorld(macarico.Env):
                 model[current_state, next_state] += action_probability
         return model
 
+    def policy_eval(self, policy, P, rewards, discount_factor=1.0, theta=0.00001):
+        """
+        Evaluate a policy given an environment and a full description of the environment's dynamics.
+
+        Args:
+            policy: [S, A] shaped matrix representing the policy.
+            env: OpenAI env. env.P represents the transition probabilities of the environment.
+                env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+                env.nS is a number of states in the environment.
+                env.nA is a number of actions in the environment.
+            theta: We stop evaluation once our value function change is less than theta for all states.
+            discount_factor: Gamma discount factor.
+
+        Returns:
+            Vector of length env.nS representing the value function.
+        """
+        import numpy as np
+        # Start with a random (all 0) value function
+        # TODO Generalize to different number of states
+        nS = 16
+        V = np.zeros(nS)
+        while True:
+            delta = 0
+            # For each state, perform a "full backup"
+            for s in range(nS):
+                v = 0
+                # Look at the possible next actions
+                for a, action_prob in enumerate(policy[s]):
+                    # For each action, look at the possible next states...
+                    for next_state, prob in enumerate(P[s, a]):
+                        reward = rewards[s, a]
+                        # Calculate the expected value. Ref: Sutton book eq. 4.6.
+                        v += action_prob * prob * (reward + discount_factor * V[next_state])
+                # How much our value function changed (across any states)
+                delta = max(delta, np.abs(v - V[s]))
+                V[s] = v
+            # Stop evaluating once our value function change is below a threshold
+            print('delta: ', delta)
+            if delta < theta:
+                break
+        return np.array(V)
+
 
 class GridLoss(macarico.Loss):
     def __init__(self):
