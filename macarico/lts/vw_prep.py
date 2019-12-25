0ee564bf2edc1d74cdeb3280d681be51b2f1394c
amrsharaf <amr.n.sharaf@gmail.com>
2019-12-25 15:39:49 +0200

    keep track of V, Q, A for gridworld

--- macarico/lts/vw_prep.py
+++ macarico/lts/vw_prep.py
@@ -32,14 +32,12 @@ class VwPrep(BanditLOLS):
         # Contains the value differences predicted at each time-step
         self.prev_state = None
         # TODO why 200?
-        self.per_step_count = np.zeros(200)
         self.counter = 0
         self.actor = actor
         self.total_sq_loss = 0.0
         self.total_vd_sq_loss = 0.0
 
     def forward(self, state):
-        self.per_step_count[self.t] += 1
         if self.t is None or self.t == []:
             self.T = state.horizon()
             self.init_state = self.actor(state).data
@@ -71,6 +69,23 @@ class VwPrep(BanditLOLS):
         return a_pol
 
     def get_objective(self, loss0, final_state=None):
+        # TODO cleanup and generlize beyond grid-world
+        states = np.eye(16)
+        Pi = np.zeros((16, 4))
+        for i, state in enumerate(states):
+            Pi[i] = self.policy.distribution(state)
+        # Definition of rewards for gridworld
+        # Transition model
+#        print('Pi: ', Pi)
+        model = final_state.model(Pi)
+        costs = final_state.costs(Pi)
+        costs_function = final_state.costs_function()
+        transition = final_state.transition()
+        V_Pi = np.dot(np.linalg.inv(np.eye(16) - final_state.example.gamma * model), costs)
+        Q_Pi = costs_function + final_state.example.gamma * transition.dot(V_Pi)
+#        print('V_Pi[initial state]: ', V_Pi[3])
+#        print('loss0: ', loss0)
+        # For the current policy Pi, what is the distribution over different actions?
         loss0 = float(loss0)
         self.counter += 1
         terminal_loss = torch.Tensor([[final_state.loss(self.t - 1)]])
@@ -87,13 +102,18 @@ class VwPrep(BanditLOLS):
         assert self.dev_t is not None
         for dev_t, dev_a, dev_imp_weight, dev_ex, transition_ex in zip(
                 self.dev_t, self.dev_a, self.dev_imp_weight, self.dev_ex, self.transition_ex):
-            pred_vd = self.pred_act_cost[dev_t-1]
-            residual_loss = loss0 - initial_state_value - (prefix_sum[dev_t-1] - self.pred_act_cost[dev_t-1])
-            vd_sq_loss = (residual_loss - pred_vd) ** 2
-            self.total_vd_sq_loss += vd_sq_loss
-            transition_example = str(residual_loss) + transition_ex
-            self.vw_vd_regressor.learn(transition_example)
-            self.policy.update(dev_a, residual_loss, dev_imp_weight, dev_ex)
+            start_state = [float(x.split(':')[1]) for x in transition_ex.replace('|', '').strip().split()[:-1]][:16].index(1.0)
+            end_state = [float(x.split(':')[1]) for x in transition_ex.replace('|', '').strip().split()[:-1]][16:].index(1.0)
+            advantage = Q_Pi[start_state, dev_a] - V_Pi[start_state]
+#            pred_vd = self.pred_act_cost[dev_t-1]
+#            residual_loss = loss0 - initial_state_value - (prefix_sum[dev_t-1] - self.pred_act_cost[dev_t-1])
+#            vd_sq_loss = (residual_loss - pred_vd) ** 2
+#            self.total_vd_sq_loss += vd_sq_loss
+#            transition_example = str(residual_loss) + transition_ex
+#            self.vw_vd_regressor.learn(transition_example)
+#            bandit_loss = residual_loss
+            bandit_loss = advantage
+            self.policy.update(dev_a, bandit_loss, dev_imp_weight, dev_ex)
         self.vw_ref_critic.learn(initial_state_ex)
         self.t, self.dev_t, self.dev_a, self.dev_imp_weight, self.pred_act_cost, self.dev_ex, self.transition_ex = [None] * 7
         return loss0
